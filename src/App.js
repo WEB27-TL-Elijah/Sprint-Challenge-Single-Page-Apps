cd6c8bfd1253f39f46976b44a5dab938a71fe393
Elijah Paminsan <e.paminsan@gmail.com>
2020-02-04 08:44:38 -0800

    support hours mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import Header from "./components/Header.js";
-
+import React from 'react'
+import Header from './components/Header.js'
+import TabNav from './components/TabNav.js'
+import AppRouter from './components/AppRouter'
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+	return (
+		<main>
+			<Header />
+			<TabNav />
+			<AppRouter />
+		</main>
+	)
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import CharacterCard from './CharacterCard'
+import SearchForm from './SearchForm'
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+	// TODO: Add useState to track data from useEffect
+	const [characters, setCharacters] = useState([])
+	const [filteredData, updateData] = useState([])
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+	const search = charAt => updateData(charAt)
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+	useEffect(() => {
+		// TODO: Add API Request here - must run in `useEffect`
+		//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+		axios
+			.get('https://rickandmortyapi.com/api/character/')
+			.then(res => {
+				setCharacters(res.data.results)
+				updateData(res.data.results)
+			})
+			.catch(err => err)
+	}, [])
+
+	return (
+		<div>
+			<SearchForm characters={characters} search={search} />
+			<section className='character-list grid-view'>
+				{filteredData.map(char => {
+					return (
+						<div>
+							<CharacterCard characters={char} key={char.id} />
+						</div>
+					)
+				})}
+			</section>
+		</div>
+	)
 }
